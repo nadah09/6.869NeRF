2c775cc484823fa848d987ce5886dc806f0cce6e
sabrinaliu <sliu.owl68@yahoo.com>
2021-05-17 14:21:35 +0000

    Add back cuda tensor and fix range bug

--- run_nerf.py
+++ run_nerf.py
@@ -841,6 +841,6 @@ def train():
 
 
 if __name__=='__main__':
-   # torch.set_default_tensor_type('torch.cuda.FloatTensor')
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
     with torch.autograd.set_detect_anomaly(True):
         train()
--- run_nerf_helpers.py
+++ run_nerf_helpers.py
@@ -161,7 +161,7 @@ def get_rays(H, W, focal, c2w):
     '''
     Get ray origins, directions from a pinhole camera
     '''
-    j, i = torch.meshgrid(torch.range(W, dtype = torch.float32), torch.range(H, dtype = torch.float32)) # indexing reversed of tf
+    j, i = torch.meshgrid(torch.range(start=0, end=W-1, dtype = torch.float32), torch.range(start=0, end=H-1, dtype = torch.float32)) # indexing reversed of tf
     dirs = torch.stack([(i-W*.5)/focal, -(j-H*.5)/focal, -torch.ones_like(i)], -1)
     rays_d = torch.sum(dirs[..., np.newaxis, :] * c2w[:3, :3], -1)
     rays_o = torch.broadcast_to(c2w[:3, -1], list(rays_d.size()))
