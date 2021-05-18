6e1af69a6778bb0034994e9bd9e83d1cdfc006de
sabrinaliu <sliu.owl68@yahoo.com>
2021-05-18 17:31:33 +0000

    Fix bugs in load_blender for synthetic images and add config file for lego

--- load_blender.py
+++ load_blender.py
@@ -4,6 +4,8 @@ import torchvision
 import numpy as np
 import imageio
 import json
+import cv2
+
 
 trans_t = lambda t : torch.tensor([[1, 0, 0, 0],
                                    [0, 1, 0, 0],
@@ -20,8 +22,8 @@ rot_theta = lambda th : torch.tensor([[np.cos(th), 0, -np.sin(th), 0],
                                       [np.sin(th), 0, np.cos(th), 0],
                                       [0, 0, 0, 1]], dtype=torch.float32)
 
-def pose_spherical(theta, pi, radius):
-    c2w = np.array([[-1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]]) @\
+def pose_spherical(theta, phi, radius):
+    c2w = torch.Tensor(np.array([[-1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])) @\
      rot_theta(theta/180.0*np.pi) @ rot_phi(phi/180.0*np.pi) @ trans_t(radius)
     return c2w
 
@@ -29,7 +31,7 @@ def load_blender_data(basedir, half_res = False, testskip = 1):
     splits = ['train', 'val', 'test']
     metas = {}
     for s in splits:
-        with open(os.path(basedir, 'transforms_{}.json'.format(s)), 'r') as fp:
+        with open(os.path.join(basedir, 'transforms_{}.json'.format(s)), 'r') as fp:
             metas[s] = json.load(fp)
 
     all_imgs = []
@@ -47,7 +49,7 @@ def load_blender_data(basedir, half_res = False, testskip = 1):
         for frame in meta['frames'][::skip]:
             fname = os.path.join(basedir, frame['file_path'] + '.png')
             imgs.append(imageio.imread(fname))
-            poses.append(np.array(fname['transform_matrix']))
+            poses.append(np.array(frame['transform_matrix']))
         imgs = (np.array(imgs) / 255.0).astype(np.float32)
         poses = np.array(poses).astype(np.float32)
         counts.append(counts[-1] + imgs.shape[0])
@@ -66,11 +68,19 @@ def load_blender_data(basedir, half_res = False, testskip = 1):
     render_poses = torch.stack([pose_spherical(angle, -30.0, 4.0) for angle in np.linspace(-180, 180, 41)[:-1]], dim=0)
 
     if half_res:
-        m = torch.nn.AvgPool2d(2)
-        for idx in range(imgs.shape[0]):
-            imgs[idx,:,:,:] = m(imgs[idx])
-
+        #m = torch.nn.AvgPool2d(2)
+        #for idx in range(imgs.shape[0]):
+        #    imgs[idx,:,:,:] = m(imgs[idx])
+        
         H, W = imgs[0].shape[:2]
-        focal = focal / 2.0
+        focal = focal/2.0
+
+        imgs_half_res = np.zeros((imgs.shape[0], H, W, 4))
+        for i, img in enumerate(imgs):
+            imgs_half_res[i] = cv2.resize(img, (H, W), interpolation=cv2.INTER_AREA)
+            imgs = imgs_half_res
+
+        #H, W = imgs[0].shape[:2]
+        #focal = focal / 2.0
 
     return imgs, poses, render_poses, [H, W, focal], i_split
